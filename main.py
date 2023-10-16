9c5a11b4c25c05fcab39f20aa13dff22a7b1b8dd
Jorge Anais <jrganais@gmail.com>
2023-10-16 00:20:38 -0300

    First implementation of the merging algorithm

--- main.py
+++ main.py
@@ -1,5 +1,39 @@
+import click
 from pathlib import Path
 
-datapath = Path("/run/media/jorge/BLUE/DATA/phd/vvvx")
+from astropy.table import Table
+
+from mesk.merge import merge_tables
+
+"""
+Program that merges two fits tables containing photometrical data into one table.
+Example:
+    python main.py \
+    -f1 /run/media/jorge/BLUE/DATA/phd/vvvx/jhk0512.fits \
+    -f2 /run/media/jorge/BLUE/DATA/phd/vvvx/jhk0511.fits \
+    -o restuls.fits
+"""
 
 
+@click.command()
+@click.option("-f1", help="File of the input fits table 1", required=True)
+@click.option("-f2", help="File of the input fits table 2", required=True)
+@click.option("-o", help="Output file for the merged table", required=True)
+def main(f1, f2, o):
+    """
+    Program that merges two fits tables containing photometrical data into one table.
+    """
+
+    file1 = Path(f1)
+    file2 = Path(f2)
+    output_file = Path(o)
+
+    table1 = Table.read(file1)
+    table2 = Table.read(file2)
+
+    merged_table = merge_tables(table1, table2)
+    merged_table.write(output_file, format="fits")
+
+
+if __name__ == "__main__":
+    main()
--- mesk/merge.py
+++ mesk/merge.py
@@ -5,33 +5,80 @@ from astropy import units as u
 import numpy as np
 import numpy.typing as npt
 
+
+MATCHED_COLUMN_SUFFIX = ["a", "b"]
+
+
 def _table_to_array(t: Table, columns: list[str]) -> npt.NDArray:
     """Convert columns of an astropy table into a numpy array"""
-    
+
     output_array = np.zeros(shape=(len(t), len(columns)))
     for i, col in enumerate(columns):
         output_array[:, i] = t[col].data.data
-    
+
     return output_array
 
+
 def _table_to_marray(t: Table, columns: list[str]) -> tuple[npt.NDArray, npt.NDArray]:
-    """Convert columns of an astropy table into a numpy masked array"""
-    
+    """Convert columns of an astropy table into a numpy array and a mask"""
+
     output_array = np.zeros(shape=(len(t), len(columns)))
     output_mask = np.ma.empty(shape=(len(t), len(columns)))
     for i, col in enumerate(columns):
         output_array[:, i] = t[col].data.data
         output_mask[:, i] = t[col].data.mask
-    
+
     return output_array, output_mask
 
+
+def compute_weighted_magnitude_and_error(
+    table: Table, magnitude_cols: list[str, str], error_cols: list[str, str]
+) -> tuple[npt.NDArray, npt.NDArray]:
+    """
+    Compute the weighted average of two magnitudes from a astropy table.
+
+    | mag-A | error-mag-A | mag-B | error-mag-B |
+    |-------|-------------|-------|-------------|
+    |  ...  |     ...     |  ...  |     ...     |
+
+    Parameters
+    ----------
+    table : Table
+        Table with the magnitudes.
+    magnitude_colnames : list[str, str]
+        Names of the columns with the magnitudes.
+    error_colnames : list[str, str]
+        Names of the columns with the errors.
+
+    Returns
+    -------
+    tuple[Column, Column]
+        Weighted average of the magnitudes and the errors.
+
+    """
+
+    magnitudes, m_mask = _table_to_marray(table, magnitude_cols)
+    magerrors, e_mask = _table_to_marray(table, error_cols)
+
+    # First, compute the weighted average of the magnitudes
+    masked_magnitudes = np.ma.masked_array(magnitudes, mask=m_mask)
+    weights = 1.0 / np.power(magerrors, 2.0)
+    weighted_magnitude = np.ma.average(masked_magnitudes, weights=weights, axis=1)
+
+    # Second, compute the error of the weighted average
+    weighted_magnitude_error = np.sqrt(
+        1.0
+        / np.sum(np.ma.masked_array(np.power(1.0 / magerrors, 2), mask=e_mask), axis=1)
+    )
+
+    return weighted_magnitude, weighted_magnitude_error
 
 
 def match_tables(
     table1: Table,
     table2: Table,
     tol_arcsec: float = 0.34,
-) -> Table:
+) -> tuple[Table, Table]:
     """Match two tables based on the values of two columns.
 
     Parameters
@@ -45,30 +92,21 @@ def match_tables(
 
     Returns
     -------
-    Table
-        Table with the matched rows.
+    tuple[Table, Table]
+        Tuple containing tables with unmatched sources and matched sources.
 
     """
 
-    
+    table1 = table1.copy()
+    table2 = table2.copy()
+
     # Check if the tables have the same columns
     if set(table1.colnames) != set(table2.colnames):
         raise ValueError("The tables have different columns")
-    
-    magnitude_colnames = [col for col in table1.colnames if "mag" in col]
-    error_colnames = [col for col in table1.colnames if "err" in col]
 
-    c1 = SkyCoord(
-        ra=table1["ra"],
-        dec=table1["dec"],
-        unit=(u.deg, u.deg)
-    )
+    c1 = SkyCoord(ra=table1["ra"], dec=table1["dec"], unit=(u.deg, u.deg))
 
-    c2 = SkyCoord(
-        ra=table2["ra"],
-        dec=table2["dec"],
-        unit=(u.deg, u.deg)
-    )
+    c2 = SkyCoord(ra=table2["ra"], dec=table2["dec"], unit=(u.deg, u.deg))
 
     idx, d2d, _ = c1.match_to_catalog_sky(c2)
     matched_indx = d2d < tol_arcsec * u.arcsec
@@ -80,35 +118,130 @@ def match_tables(
     unmatched_sources = vstack([table1[~matched_indx], table2[~matched_indx_c2]])
 
     # Combine matched rows
-    aux_matched_sources = hstack(
-        [
-            table1[matched_indx],
-            table2[idx[matched_indx]]
-        ],
-        table_names=["a", "b"]
+    matched_sources = hstack(
+        [table1[matched_indx], table2[idx[matched_indx]]],
+        table_names=MATCHED_COLUMN_SUFFIX,
     )
 
-    # Missing values treatment. Magnitudes are computed using 
-    # columns = ["mag_1_a", "mag_1_b"]
-    # x = _table_to_array(aux_matched_sources, ["mag_1_a", "mag_1_b"])
-    # w = 1./np.power(_table_to_array(aux_matched_sources, ["er_1_a", "er_1_a"]),2)
-    # np.average(x, weights=w, axis=1)
+    return unmatched_sources, matched_sources
 
 
-    # Using masked arrays
-    columns = ["mag_1_a", "mag_1_b"]
+def avg_positions(
+    table: Table,
+    lon: str = "ra",
+    lat: str = "dec",
+) -> Table:
+    """Average the positions (lon, lat) from matched sources."""
 
-    x, mask = _table_to_marray(aux_matched_sources, ["mag_1_a", "mag_1_b"])
-    x_masked = np.ma.masked_array(x, mask=mask)
-    w = 1./np.power(_table_to_array(aux_matched_sources, ["er_1_a", "er_1_b"]),2)
+    def _get_average_value(
+        table: Table,
+        col: str,
+    ):
+        """Get the average value of two columns from a table (small angle aproximation)"""
+        x_deg = _table_to_array(
+            table,
+            [f"{col}_{MATCHED_COLUMN_SUFFIX[0]}", f"{col}_{MATCHED_COLUMN_SUFFIX[1]}"],
+        )
+        x_rad = np.deg2rad(x_deg)
+        x_rad_mean = np.average(x_rad, axis=1)
+        return np.rad2deg(x_rad_mean)
 
-    result = np.ma.average(x_masked, weights=w, axis=1)
+    table = table.copy()
+    table[lon] = _get_average_value(table, lon)
+    table[lat] = _get_average_value(table, lat)
 
-    # Error calculation
-    xer, xer_mask = _table_to_marray(aux_matched_sources, ["er_1_a", "er_1_b"])
-    result_error = np.sqrt(1. / np.sum(np.ma.masked_array(np.power(1. / xer, 2), mask=xer_mask), axis=1))
-    
+    table.remove_columns(
+        [
+            f"{lon}_{MATCHED_COLUMN_SUFFIX[0]}",
+            f"{lon}_{MATCHED_COLUMN_SUFFIX[1]}",
+            f"{lat}_{MATCHED_COLUMN_SUFFIX[0]}",
+            f"{lat}_{MATCHED_COLUMN_SUFFIX[1]}",
+        ]
+    )
 
-    aux_matched_sources["result"] = result
-    aux_matched_sources["result_error"] = result_error
-    aux_matched_sources.write("partialresults.fits", overwrite=True)
+    return table
+
+
+def avg_magnitudes(
+    table: Table,
+    band_names: list[str],
+    error_names: list[str],
+) -> Table:
+    """
+    Combine the magnitudes from matched sources for different bands using weighted average.
+
+    Parameters
+    ----------
+
+    table : Table
+        Table with the matched sources.
+    band_names : list[str]
+        Names of the bands.
+    error_names : list[str]
+        Names of the errors.
+
+    Returns
+    -------
+    Table
+        Table with the combined magnitudes.
+    """
+
+    table = table.copy()
+
+    for band_name, error_name in zip(band_names, error_names):
+        w_mag, w_error = compute_weighted_magnitude_and_error(
+            table,
+            magnitude_cols=[
+                f"{band_name}_{MATCHED_COLUMN_SUFFIX[0]}",
+                f"{band_name}_{MATCHED_COLUMN_SUFFIX[1]}",
+            ],
+            error_cols=[
+                f"{error_name}_{MATCHED_COLUMN_SUFFIX[0]}",
+                f"{error_name}_{MATCHED_COLUMN_SUFFIX[1]}",
+            ],
+        )
+
+        # Remove the columns with the magnitudes and errors
+        table.remove_columns(
+            [
+                f"{band_name}_{MATCHED_COLUMN_SUFFIX[0]}",
+                f"{band_name}_{MATCHED_COLUMN_SUFFIX[1]}",
+            ]
+        )
+        table.remove_columns(
+            [
+                f"{error_name}_{MATCHED_COLUMN_SUFFIX[0]}",
+                f"{error_name}_{MATCHED_COLUMN_SUFFIX[1]}",
+            ]
+        )
+
+        # Add the new columns with the weighted magnitudes and errors
+        table[f"{band_name}"] = w_mag
+        table[f"{error_name}"] = w_error
+
+    return table
+
+
+def merge_tables(
+    table1: Table,
+    table2: Table,
+) -> Table:
+    table1 = table1.copy()
+    table2 = table2.copy()
+
+    # Get column names
+    magnitude_colnames = [col for col in table1.colnames if "mag" in col]
+    error_colnames = [col for col in table1.colnames if "er" in col]
+
+    # Match and combine tables
+    unmatched_sources_t, matched_sources_t = match_tables(table1, table2)
+
+    matched_sources_t = avg_magnitudes(
+        table=matched_sources_t,
+        band_names=magnitude_colnames,
+        error_names=error_colnames,
+    )
+
+    matched_sources_t = avg_positions(matched_sources_t)
+
+    return vstack([unmatched_sources_t, matched_sources_t])
